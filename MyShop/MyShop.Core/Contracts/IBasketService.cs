87da67dbdc4d614c1acf6c66b8b87d148311fe36
Ogu Iheanyichukwu Princewill <fecs.prince@gmail.com>
2020-07-12 21:19:21 +0100

    BasketService

---
+++ MyShop/MyShop.Core/Contracts/IBasketService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web;
+using System.Threading.Tasks;
+using MyShop.Core.ViewModels;
+
+namespace MyShop.Core.Contracts
+{   
+   public interface IBasketService
+    {
+        void AddToBasket(HttpContextBase httpContext, string productId);
+        void RemoveBasket(HttpContextBase httpContext, string itemId);
+        List<BasketItemViewModel> GetBasketItem(HttpContextBase httpContext);
+        BasketSummaryViewModel GetBasketSummary(HttpContextBase httpContext);
+    }
+}
---
+++ MyShop/MyShop.Services/BasketService.cs
@@ -0,0 +1,162 @@
+﻿using MyShop.Core.Contracts;
+using MyShop.Core.Models;
+using MyShop.Core.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace MyShop.Services
+{
+    public class BasketService : IBasketService
+    {
+        IRepository<Product> productContext;
+        IRepository<Basket> basketContext;
+
+        public const string BasketServiceName = "eCommerceBaket";
+
+        //CLASS CONSTRUCTOR
+        public BasketService(IRepository<Product> ProductContext, IRepository<Basket> BasketContext)
+        {
+            this.basketContext = BasketContext;
+            this.productContext = ProductContext;
+        }
+
+        //GETTING BASKET METHOD
+        private Basket GetBasket(HttpContextBase httpContext, bool createIfNull)
+        {
+            HttpCookie cookie = httpContext.Request.Cookies.Get(BasketServiceName);
+
+            Basket basket = new Basket();
+            if (cookie != null)
+            {
+                string basketId = cookie.Value;
+                if (!string.IsNullOrEmpty(basketId))
+                {
+                    basket = basketContext.Find(basketId);
+                }
+                else
+                {
+                    if (createIfNull)
+                    {
+                        basket = CreateNewBasket(httpContext);
+                    }
+                }
+            }
+            else
+            {
+                if (createIfNull)
+                {
+                    basket = CreateNewBasket(httpContext);
+                }
+
+            }
+            return basket;
+        }
+
+        //CREATING NEW BASKET METHOD
+        private Basket CreateNewBasket(HttpContextBase httpContext)
+        {
+            Basket basket = new Basket();
+            basketContext.Insert(basket);
+            basketContext.Commit();
+
+            HttpCookie cookie = new HttpCookie(BasketServiceName);
+            cookie.Value = basket.Id;
+
+            cookie.Expires = DateTime.Now.AddDays(1);
+            httpContext.Response.Cookies.Add(cookie);
+
+            return basket;
+        }
+
+        //ADDING ITEM IN BASKET METHOD
+        public void AddToBasket(HttpContextBase httpContext, string productId)
+        {
+            Basket basket = GetBasket(httpContext, true);
+            BasketItem item = basket.BasketItems.FirstOrDefault(i => i.ProductId == productId);
+
+            if (item == null)
+            {
+                item = new BasketItem()
+                {
+                    BasketId = basket.Id,
+                    ProductId = productId,
+                    Quantity = 1
+                };
+                basket.BasketItems.Add(item);
+            }
+            else
+            {
+                item.Quantity = item.Quantity + 1;
+            }
+            basketContext.Commit();
+        }
+
+        //REMOVING ITEM FROM BASKET METHOD
+        public void RemoveBasket(HttpContextBase httpContext, string itemId)
+        {
+            Basket basket = GetBasket(httpContext, true);
+            BasketItem item = basket.BasketItems.FirstOrDefault(i => i.Id == itemId);
+
+            if (item != null)
+            {
+                basket.BasketItems.Remove(item);
+                basketContext.Commit();
+            }
+
+        }
+
+        public List<BasketItemViewModel> GetBasketItem(HttpContextBase httpContext)
+        {
+            Basket basket = GetBasket(httpContext, false);
+
+            if (basket != null)
+            {
+                var result = (from b in basket.BasketItems join 
+                              p in productContext.Collection() 
+                              on b.ProductId equals p.Id
+                              select new BasketItemViewModel()
+                              {
+                                  Id = b.Id,
+                                  ProductName = p.Name,
+                                  Price = p.Price,
+                                  Quantity = b.Quantity,
+                                  Image = p.Image
+                              }).ToList();
+
+                return result;
+            }
+            return new List<BasketItemViewModel>();
+        }
+
+        public BasketSummaryViewModel GetBasketSummary(HttpContextBase httpContext)
+        {
+            Basket basket = GetBasket(httpContext, false);
+            BasketSummaryViewModel model = new BasketSummaryViewModel(0, 0); 
+
+            if (basket != null)
+            {
+                int? basketCount = (from a in basket.BasketItems
+                                    select a.Quantity).Sum();
+
+                decimal? basketTotal = (from item in basket.BasketItems join
+                                       prod in productContext.Collection() on
+                                       item.ProductId equals prod.Id
+                                       select item.Quantity * prod.Price).Sum();
+
+                model.BasketCount = basketCount ?? 0;
+                model.BasketTotalValue = basketTotal ?? decimal.Zero;
+
+                return model;
+
+            }
+            else
+            {
+                return model;
+            }
+        }
+    }
+}
